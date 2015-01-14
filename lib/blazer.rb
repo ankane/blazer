139057bec87bae06747ea982376eecdfa8300d7d
Paul Tyng <paul@paultyng.net>
2015-01-14 09:08:36 -0500

    User initializer variable for user class name

--- lib/blazer.rb
+++ lib/blazer.rb
@@ -8,11 +8,13 @@ module Blazer
     attr_accessor :audit
     attr_reader :time_zone
     attr_accessor :user_name
+    attr_accessor :user_class
     attr_accessor :timeout
   end
   self.audit = true
   self.user_name = :name
   self.timeout = 15
+  self.user_class = 'User'
 
   def self.time_zone=(time_zone)
     @time_zone = time_zone.is_a?(ActiveSupport::TimeZone) ? time_zone : ActiveSupport::TimeZone[time_zone.to_s]

db353a4cc32b89bcf35504a5d7eec29b28c78bc4
Andrew Kane <acekane1@gmail.com>
2015-07-22 00:08:15 -0700

    Do not throw error out of the box if no user class is defined

--- lib/blazer.rb
+++ lib/blazer.rb
@@ -14,7 +14,6 @@ module Blazer
   self.audit = true
   self.user_name = :name
   self.timeout = 15
-  self.user_class = "User"
 
   def self.time_zone=(time_zone)
     @time_zone = time_zone.is_a?(ActiveSupport::TimeZone) ? time_zone : ActiveSupport::TimeZone[time_zone.to_s]
--- lib/blazer/engine.rb
+++ lib/blazer/engine.rb
@@ -7,6 +7,8 @@ module Blazer
       app.config.assets.precompile << proc { |path| path =~ /\Ablazer\/application\.(js|css)\z/ }
 
       Blazer.time_zone ||= Time.zone
+      Blazer.user_class ||= User rescue nil
+      Blazer::Query.belongs_to :creator, class_name: Blazer.user_class.to_s if Blazer.user_class
     end
   end
 end

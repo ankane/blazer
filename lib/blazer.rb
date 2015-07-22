e8c301a2b475e090c65ab890d32af46cb888c8fb
Andrew Kane <acekane1@gmail.com>
2015-07-22 00:00:14 -0700

    Set time zone after Rails initializes

--- lib/blazer.rb
+++ lib/blazer.rb
@@ -19,5 +19,4 @@ module Blazer
   def self.time_zone=(time_zone)
     @time_zone = time_zone.is_a?(ActiveSupport::TimeZone) ? time_zone : ActiveSupport::TimeZone[time_zone.to_s]
   end
-  self.time_zone = Time.zone
 end
--- lib/blazer/engine.rb
+++ lib/blazer/engine.rb
@@ -2,9 +2,11 @@ module Blazer
   class Engine < ::Rails::Engine
     isolate_namespace Blazer
 
-    initializer "precompile" do |app|
+    initializer "blazer" do |app|
       # use a proc instead of a string
       app.config.assets.precompile << proc { |path| path =~ /\Ablazer\/application\.(js|css)\z/ }
+
+      Blazer.time_zone = Time.zone
     end
   end
 end

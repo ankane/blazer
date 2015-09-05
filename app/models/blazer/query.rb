a8fdedf8388ae1225bf1b9a3321b271c54749c85
Andrew Kane <acekane1@gmail.com>
2015-09-05 10:47:26 -0700

    Development reload fix

--- app/models/blazer/query.rb
+++ app/models/blazer/query.rb
@@ -1,5 +1,7 @@
 module Blazer
   class Query < ActiveRecord::Base
+    belongs_to :creator, class_name: Blazer.user_class.to_s if Blazer.user_class
+
     validates :name, presence: true
     validates :statement, presence: true
 

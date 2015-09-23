25250d98ac3a1fc499ad05fb27892bb7b6a19aaf
Ermolaev Andrey <andrey@ermolaev.me>
2015-09-24 02:52:56 +0300

    Blazer.user_class always is string

--- lib/blazer/engine.rb
+++ lib/blazer/engine.rb
@@ -7,7 +7,7 @@ module Blazer
       app.config.assets.precompile << proc { |path| path =~ /\Ablazer\/application\.(js|css)\z/ }
 
       Blazer.time_zone ||= Time.zone
-      Blazer.user_class ||= User rescue nil
+      Blazer.user_class ||= User.to_s rescue nil
 
       if Blazer.user_class
         Blazer.current_user_name = "current_#{Blazer.user_class.downcase.singularize}"

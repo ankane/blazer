e5880612021d984e908a9266c4c306af532b1a0e
Andrew Kane <acekane1@gmail.com>
2015-10-02 00:07:25 -0700

    Explain config

--- lib/blazer/engine.rb
+++ lib/blazer/engine.rb
@@ -6,8 +6,11 @@ module Blazer
       # use a proc instead of a string
       app.config.assets.precompile << proc { |path| path =~ /\Ablazer\/application\.(js|css)\z/ }
 
-      Blazer.time_zone ||= Time.zone
-      Blazer.user_class ||= User rescue nil
+      Blazer.time_zone ||= Blazer.settings["time_zone"] || Time.zone
+      Blazer.audit = Blazer.settings.key?("audit") ? Blazer.settings["audit"] : true
+      Blazer.user_name = Blazer.settings["user_name"] if Blazer.settings["user_name"]
+
+      Blazer.user_class ||= Blazer.settings["user_class"] || User rescue nil
       Blazer::Query.belongs_to :creator, class_name: Blazer.user_class.to_s if Blazer.user_class
     end
   end

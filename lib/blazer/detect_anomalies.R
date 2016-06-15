4a9979fda23d3e7595bb19bbbd239c5229dc3e45
Andrew Kane <acekane1@gmail.com>
2016-06-15 00:45:17 -0700

    Catch all errors

--- lib/blazer/detect_anomalies.R
+++ lib/blazer/detect_anomalies.R
@@ -1,12 +1,12 @@
-library(AnomalyDetection)
-
-args <- commandArgs(trailingOnly = TRUE)
-
-con <- textConnection(args[1])
-data <- read.csv(con, stringsAsFactors = FALSE)
-data$timestamp <- as.POSIXct(data$timestamp)
-
 tryCatch({
+  library(AnomalyDetection)
+
+  args <- commandArgs(trailingOnly = TRUE)
+
+  con <- textConnection(args[1])
+  data <- read.csv(con, stringsAsFactors = FALSE)
+  data$timestamp <- as.POSIXct(data$timestamp)
+
   res = AnomalyDetectionTs(data, direction = "both", alpha = 0.05)
   write.csv(res$anoms)
 }, error = function (e) {

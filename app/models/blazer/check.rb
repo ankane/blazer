19954ba94a8b5ae4736f7de2345d9c6d13dd6ff6
Andrew Kane <acekane1@gmail.com>
2016-06-14 22:14:27 -0700

    Send message in mailer

--- app/models/blazer/check.rb
+++ app/models/blazer/check.rb
@@ -62,7 +62,7 @@ module Blazer
 
       # do not notify on creation, except when not passing
       if (state_was || state != "passing") && state != state_was && emails.present?
-        Blazer::CheckMailer.state_change(self, state, state_was, rows.size, error).deliver_later
+        Blazer::CheckMailer.state_change(self, state, state_was, rows.size, message).deliver_later
       end
       save! if changed?
     end

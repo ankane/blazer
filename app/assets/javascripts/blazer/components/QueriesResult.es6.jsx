63db1cea21d60f6265652e6dc6b8526f1d6e113c
Andrew Kane <acekane1@gmail.com>
2016-08-03 02:13:51 -0700

    Updated charts

--- app/assets/javascripts/blazer/components/QueriesResult.es6.jsx
+++ app/assets/javascripts/blazer/components/QueriesResult.es6.jsx
@@ -44,6 +44,16 @@ class QueriesResult extends React.Component {
         new Chartkick.ColumnChart(this._chartDiv, data, chartOptions)
       } else if (chart_type === "bar2") {
 
+//       <% first_20 = @rows.group_by { |r| r[0] }.values.first(20).flatten(1) %>
+//       <% labels = first_20.map { |r| r[0] }.uniq %>
+//       <% series = first_20.map { |r| r[1] }.uniq %>
+//       <% labels.each do |l| %>
+//         <% series.each do |s| %>
+//           <% first_20 << [l, s, 0] unless first_20.find { |r| r[0] == l && r[1] == s } %>
+//         <% end %>
+//       <% end %>
+//       <%= column_chart first_20.group_by { |r| v = r[1]; (@boom[@columns[1]] || {})[v.to_s] || v }.each_with_index.map { |(name, v), i| {name: name, data: v.sort_by { |r2| labels.index(r2[0]) }.map { |v2| v3 = v2[0]; [(@boom[@columns[0]] || {})[v3.to_s] || v3, v2[2]] }} }, id: chart_id %>
+
       }
     }
   }
@@ -146,25 +156,12 @@ class QueriesResult extends React.Component {
   }
 
   renderChart() {
-    const { rows, chart_type } = this.props
+    const { rows, chart_type, only_chart, markers } = this.props
+    let maps = false
 
     if (rows.length > 0) {
-      if (chart_type) {
-        return <div className="chart-div" ref={(n) => this._chartDiv = n}></div>
-      }
+      if (maps && markers.length > 0) {
 
-
-//     <% values = @rows.first %>
-//     <% chart_id = SecureRandom.hex %>
-//     <% column_types = @result.column_types %>
-//     <% chart_type = @result.chart_type %>
-//     <% chart_options = {id: chart_id, min: nil} %>
-//     <% series_library = {} %>
-//     <% target_index = @columns.index { |k| k.downcase == "target" } %>
-//     <% if target_index %>
-//       <% series_library[target_index - 1] = {pointStyle: "line", hitRadius: 5, borderColor: "#109618", pointBackgroundColor: "#109618", backgroundColor: "#109618"} %>
-//     <% end %>
-//     <% if blazer_maps? && @markers.any? %>
 //       <div id="map" style="height: <%= @only_chart ? 300 : 500 %>px;"></div>
 //       <script>
 //         L.mapbox.accessToken = '<%= ENV["MAPBOX_ACCESS_TOKEN"] %>';
@@ -193,35 +190,21 @@ class QueriesResult extends React.Component {
 //         featureLayer.setGeoJSON(geojson);
 //         map.fitBounds(featureLayer.getBounds());
 //       </script>
-//     <% elsif chart_type == "line" %>
-//       <%= line_chart @columns[1..-1].each_with_index.map{ |k, i| {name: k, data: @rows.map{ |r| [r[0], r[i + 1]] }, library: series_library[i]} }, chart_options %>
-//     <% elsif chart_type == "line2" %>
-//       <%= line_chart @rows.group_by { |r| v = r[1]; (@boom[@columns[1]] || {})[v.to_s] || v }.each_with_index.map { |(name, v), i| {name: name, data: v.map { |v2| [v2[0], v2[2]] }, library: series_library[i]} }, chart_options %>
-//     <% elsif chart_type == "bar" %>
-//       <%= column_chart (values.size - 1).times.map { |i| name = @columns[i + 1]; {name: name, data: @rows.first(20).map { |r| [(@boom[@columns[0]] || {})[r[0].to_s] || r[0], r[i + 1]] } } }, id: chart_id %>
-//     <% elsif chart_type == "bar2" %>
-//       <% first_20 = @rows.group_by { |r| r[0] }.values.first(20).flatten(1) %>
-//       <% labels = first_20.map { |r| r[0] }.uniq %>
-//       <% series = first_20.map { |r| r[1] }.uniq %>
-//       <% labels.each do |l| %>
-//         <% series.each do |s| %>
-//           <% first_20 << [l, s, 0] unless first_20.find { |r| r[0] == l && r[1] == s } %>
-//         <% end %>
-//       <% end %>
-//       <%= column_chart first_20.group_by { |r| v = r[1]; (@boom[@columns[1]] || {})[v.to_s] || v }.each_with_index.map { |(name, v), i| {name: name, data: v.sort_by { |r2| labels.index(r2[0]) }.map { |v2| v3 = v2[0]; [(@boom[@columns[0]] || {})[v3.to_s] || v3, v2[2]] }} }, id: chart_id %>
-//     <% elsif @only_chart %>
-//       <% if @rows.size == 1 && @rows.first.size == 1 %>
-//         <% v = @rows.first.first %>
-//         <% if v.is_a?(String) && v == "" %>
-//           <div class="text-muted">empty string</div>
-//         <% else %>
-//           <p style="font-size: 160px;"><%= blazer_format_value(@columns.first, v) %></p>
-//         <% end %>
-//       <% else %>
-//         <% @no_chart = true %>
-//       <% end %>
-//     <% end %>
 
+      } else if (chart_type) {
+        return <div className="chart-div" ref={(n) => this._chartDiv = n}></div>
+      } else if (only_chart) {
+        if (rows.length === 1 && columns.length === 1) {
+          v = rows[0][0]
+          if (typeof v === "string" && v === "") {
+            return <div className="text-muted">empty string</div>
+          } else {
+            return <p style={{fontSize: "160px"}}>{this.formatValue(columns[0], v)}</p>
+          }
+        } else {
+          this.noChart = true
+        }
+      }
     }
   }
 
@@ -281,7 +264,7 @@ class QueriesResult extends React.Component {
     let ele
 
     if (rows.length > 0) {
-      if (!only_chart || !chart_type) {
+      if (!only_chart || this.noChart) {
         // TODO better equals
         if (JSON.stringify(columns) === JSON.stringify(["QUERY PLAN"])) {
           ele = <pre><code>{rows.map((r) => r[0]).join("\n")}</code></pre>

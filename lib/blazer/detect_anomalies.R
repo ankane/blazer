2602c1f6c1f55aa5eb7ca50f4476c509f4576767
Andrew Kane <acekane1@gmail.com>
2016-08-30 08:53:15 -0700

    Revert "Allow higher number of anomalies"
    This reverts commit 4e683fc3d448aa6b91828f90cb1db92219afefeb.

--- lib/blazer/detect_anomalies.R
+++ lib/blazer/detect_anomalies.R
@@ -8,9 +8,9 @@ tryCatch({
   data$timestamp <- as.POSIXct(data$timestamp)
 
   if (identical(args[1], "ts")) {
-    res <- AnomalyDetectionTs(data, direction = "both", alpha = 0.05, max_anoms = 0.3)
+    res <- AnomalyDetectionTs(data, direction = "both", alpha = 0.05, max_anoms = 0.2)
   } else {
-    res <- AnomalyDetectionVec(data$count, direction = "both", alpha = 0.05, max_anoms = 0.3, period = length(data$count) / 2 - 1)
+    res <- AnomalyDetectionVec(data$count, direction = "both", alpha = 0.05, max_anoms = 0.2, period = length(data$count) / 2 - 1)
   }
 
   write.csv(res$anoms)

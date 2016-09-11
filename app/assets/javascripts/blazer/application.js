bf973e9cdd55b3f4ef77067ee5e38d2207ada4b7
Andrew Kane <acekane1@gmail.com>
2016-09-10 19:03:44 -0700

    Bind code toggle once

--- app/assets/javascripts/blazer/application.js
+++ app/assets/javascripts/blazer/application.js
@@ -33,6 +33,10 @@ $(document).on("change", "#bind input, #bind select", function () {
   submitIfCompleted($(this).closest("form"));
 });
 
+$(document).on("click", "#code", function () {
+  $(this).toggleClass("expanded");
+});
+
 function uuid() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);

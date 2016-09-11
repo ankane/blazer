d7e32aa9744924424408fcb284b6afc280e98219
Andrew Kane <acekane1@gmail.com>
2016-09-10 19:02:23 -0700

    Moved js to external file

--- app/assets/javascripts/blazer/application.js
+++ app/assets/javascripts/blazer/application.js
@@ -29,6 +29,10 @@ $(document).on("submit", "form[method=get]", function() {
   return false;
 });
 
+$(document).on("change", "#bind input, #bind select", function () {
+  submitIfCompleted($(this).closest("form"));
+});
+
 function uuid() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);

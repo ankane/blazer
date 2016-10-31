16e6691b26366e00a5611e1ca29255c08328a5ec
Andrew Kane <acekane1@gmail.com>
2016-10-30 19:23:40 -0700

    Fixed inherit_smart_settings

--- lib/blazer/result.rb
+++ lib/blazer/result.rb
@@ -23,14 +23,17 @@ module Blazer
       @boom ||= begin
         boom = {}
         columns.each_with_index do |key, i|
-          query = data_source.smart_columns[key]
-          if query
+          smart_columns_data_source =
+            ([data_source] + Array(data_source.settings["inherit_smart_settings"]).map { |ds| Blazer.data_sources[ds] }).find { |ds| ds.smart_columns[key] }
+
+          if smart_columns_data_source
+            query = smart_columns_data_source.smart_columns[key]
             res =
               if query.is_a?(Hash)
                 query
               else
                 values = rows.map { |r| r[i] }.compact.uniq
-                result = data_source.run_statement(ActiveRecord::Base.send(:sanitize_sql_array, [query.sub("{value}", "(?)"), values]))
+                result = smart_columns_data_source.run_statement(ActiveRecord::Base.send(:sanitize_sql_array, [query.sub("{value}", "(?)"), values]))
                 result.rows
               end
 

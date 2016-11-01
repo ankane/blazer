0d08c432a4b1381ae2c284117423a61add56aefa
ts-3156 <ts_3156@yahoo.co.jp>
2016-11-01 20:13:06 +0900

    suppress deprecation warnings when creating tables (#92)

--- lib/generators/blazer/templates/install.rb
+++ lib/generators/blazer/templates/install.rb
@@ -6,7 +6,7 @@ class <%= migration_class_name %> < ActiveRecord::Migration
       t.text :description
       t.text :statement
       t.string :data_source
-      t.timestamps
+      t.timestamps null: false
     end
 
     create_table :blazer_audits do |t|
@@ -14,20 +14,20 @@ class <%= migration_class_name %> < ActiveRecord::Migration
       t.references :query
       t.text :statement
       t.string :data_source
-      t.timestamp :created_at
+      t.timestamp :created_at, null: false
     end
 
     create_table :blazer_dashboards do |t|
       t.references :creator
       t.text :name
-      t.timestamps
+      t.timestamps null: false
     end
 
     create_table :blazer_dashboard_queries do |t|
       t.references :dashboard
       t.references :query
       t.integer :position
-      t.timestamps
+      t.timestamps null: false
     end
 
     create_table :blazer_checks do |t|
@@ -39,7 +39,7 @@ class <%= migration_class_name %> < ActiveRecord::Migration
       t.string :check_type
       t.text :message
       t.timestamp :last_run_at
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end

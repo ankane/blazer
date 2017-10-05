31bbc459d07dda84b457e2cd7f195557570ad4e0
Alessio Fanelli <fanahova@gmail.com>
2017-10-05 11:12:24 +0200

    Give better info about failing check

--- app/mailers/blazer/slack_notifier.rb
+++ app/mailers/blazer/slack_notifier.rb
@@ -3,13 +3,13 @@ require 'slack-notifier'
 module Blazer
   class SlackNotifier
     def state_change(check, state, state_was, rows_count)
-      message = "#{check} check changed status from #{state_was} to #{state}. It now returns #{rows} rows. #{query_url(check.query_id)}"
+      message = "#{check.query.name} check changed status from #{state_was} to #{state}. It now returns #{rows} rows. #{query_url(check.query_id)}"
       notify_slack(message, check.slack_channel)
     end
 
     def failing_checks(checks)
       message = "#{pluralize(checks.size, "Check")} failing.\n"
-      checks.each { |c| message << "#{query_url(check.query_id)} (#{check.state})" }
+      checks.each { |c| message << "#{query_url(check.query_id)} (#{check.state})\n" }
       notify_slack(message, checks.first.slack_channel)
     end
 

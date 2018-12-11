e258c27eac107e61601822ff8b6c5e17e6ab527c
Andrew Kane <andrew@chartkick.com>
2018-12-11 04:08:15 -0800

    Updated Ace

--- app/assets/javascripts/blazer/ace/snippets/text.js
+++ app/assets/javascripts/blazer/ace/snippets/text.js
@@ -1,1 +1,14 @@
-ace.define("ace/snippets/text",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="text"})
+define("ace/snippets/text",["require","exports","module"], function(require, exports, module) {
+"use strict";
+
+exports.snippetText =undefined;
+exports.scope = "text";
+
+});                (function() {
+                    window.require(["ace/snippets/text"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            

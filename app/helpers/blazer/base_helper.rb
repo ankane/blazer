552bd5686194296be95e58536f2f7aa1f8d79eb3
Andrew Kane <andrew@chartkick.com>
2019-05-29 01:55:17 -0700

    No need for Rails < 4.1 patch anymore

--- app/helpers/blazer/base_helper.rb
+++ app/helpers/blazer/base_helper.rb
@@ -33,21 +33,7 @@ module Blazer
     end
 
     def blazer_js_var(name, value)
-      "var #{name} = #{blazer_json_escape(value.to_json(root: false))};".html_safe
-    end
-
-    JSON_ESCAPE = { '&' => '\u0026', '>' => '\u003e', '<' => '\u003c', "\u2028" => '\u2028', "\u2029" => '\u2029' }
-    JSON_ESCAPE_REGEXP = /[\u2028\u2029&><]/u
-
-    # Prior to version 4.1 of rails double quotes were inadventently removed in json_escape.
-    # This adds the correct json_escape functionality to rails versions < 4.1
-    def blazer_json_escape(s)
-      if Rails::VERSION::STRING < "4.1"
-        result = s.to_s.gsub(JSON_ESCAPE_REGEXP, JSON_ESCAPE)
-        s.html_safe? ? result.html_safe : result
-      else
-        json_escape(s)
-      end
+      "var #{name} = #{json_escape(value.to_json(root: false))};".html_safe
     end
 
     def blazer_series_name(k)

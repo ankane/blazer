49a4fb5a690e1fd08ef5e683ee4d49d46ea62e7d
Andrew Kane <andrew@chartkick.com>
2020-11-15 04:12:48 -0800

    Validate length

--- app/models/blazer/upload.rb
+++ app/models/blazer/upload.rb
@@ -2,7 +2,7 @@ module Blazer
   class Upload < Record
     belongs_to :creator, optional: true, class_name: Blazer.user_class.to_s if Blazer.user_class
 
-    validates :table, presence: true, uniqueness: true, format: {with: /\A[a-z0-9_]+\z/, message: "can only contain lowercase letters, numbers, and underscores"}
+    validates :table, presence: true, uniqueness: true, format: {with: /\A[a-z0-9_]+\z/, message: "can only contain lowercase letters, numbers, and underscores"}, length: {maximum: 63}
 
     def table_name
       Blazer.uploads_table_name(table)

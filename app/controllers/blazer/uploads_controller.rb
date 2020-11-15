087fc965181696a6af98514834ed56e50c89b2e8
Andrew Kane <andrew@chartkick.com>
2020-11-15 03:10:29 -0800

    Check range

--- app/controllers/blazer/uploads_controller.rb
+++ app/controllers/blazer/uploads_controller.rb
@@ -94,8 +94,7 @@ module Blazer
         column_types =
           columns.size.times.map do |i|
             values = rows.map { |r| r[i] }.uniq.compact
-            # TODO check in bigint range
-            if values.all? { |v| v.is_a?(Integer) }
+            if values.all? { |v| v.is_a?(Integer) && v >= -9223372036854775808 && v <= 9223372036854775807 }
               "bigint"
             elsif values.all? { |v| v.is_a?(Numeric) }
               "decimal"

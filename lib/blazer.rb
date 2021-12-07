da86fa4bc30d994dd96b300e205171c6110129e0
Andrew Kane <andrew@ankane.org>
2021-12-07 00:20:21 -0800

    Fixed order

--- lib/blazer.rb
+++ lib/blazer.rb
@@ -251,10 +251,10 @@ Blazer.register_adapter "elasticsearch", Blazer::Adapters::ElasticsearchAdapter
 Blazer.register_adapter "hive", Blazer::Adapters::HiveAdapter
 Blazer.register_adapter "ignite", Blazer::Adapters::IgniteAdapter
 Blazer.register_adapter "influxdb", Blazer::Adapters::InfluxdbAdapter
+Blazer.register_adapter "mongodb", Blazer::Adapters::MongodbAdapter
 Blazer.register_adapter "neo4j", Blazer::Adapters::Neo4jAdapter
 Blazer.register_adapter "opensearch", Blazer::Adapters::OpensearchAdapter
 Blazer.register_adapter "presto", Blazer::Adapters::PrestoAdapter
-Blazer.register_adapter "mongodb", Blazer::Adapters::MongodbAdapter
 Blazer.register_adapter "salesforce", Blazer::Adapters::SalesforceAdapter
 Blazer.register_adapter "soda", Blazer::Adapters::SodaAdapter
 Blazer.register_adapter "spark", Blazer::Adapters::SparkAdapter

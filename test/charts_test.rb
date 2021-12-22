ee3618dcfc94483d1f72a8c2d085ca67e43927bf
Andrew Kane <andrew@ankane.org>
2021-12-22 01:15:34 -0800

    Improved tests

---
+++ test/charts_test.rb
@@ -0,0 +1,49 @@
+require_relative "test_helper"
+
+class ControllerTest < ActionDispatch::IntegrationTest
+  # TODO fix casting
+  def test_line_chart
+    run_query "SELECT date('now'), 1"
+    # assert_match "LineChart", response.body
+  end
+
+  def test_column_chart
+    run_query "SELECT 'Label', 1"
+    assert_match "ColumnChart", response.body
+  end
+
+  def test_scatter_chart
+    run_query "SELECT 1, 2"
+    assert_match "ScatterChart", response.body
+  end
+
+  def test_pie_chart
+    run_query "SELECT 'Label', 1 AS pie"
+    assert_match "PieChart", response.body
+  end
+
+  def test_map_latitude_longitude
+    run_query "SELECT 1.2 AS latitude, 3.4 AS longitude"
+    assert_match "map", response.body
+  end
+
+  def test_map_lat_lon
+    run_query "SELECT 1.2 AS lat, 3.4 AS lon"
+    assert_match "map", response.body
+  end
+
+  def test_map_lat_lng
+    run_query "SELECT 1.2 AS lat, 3.4 AS lng"
+    assert_match "map", response.body
+  end
+
+  # TODO update
+  def test_target
+    run_query "SELECT date('now'), 1, 2 AS target"
+  end
+
+  def run_query(statement)
+    post blazer.run_queries_path, params: {statement: statement, data_source: "main"}, xhr: true
+    assert_response :success
+  end
+end
--- test/controller_test.rb
+++ test/controller_test.rb
@@ -1,8 +1,35 @@
 require_relative "test_helper"
 
 class ControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    Blazer::Audit.delete_all
+    Blazer::Query.delete_all
+  end
+
   def test_index
     get blazer.root_path
+    assert_response :success
+  end
+
+  def test_create_query
+    post blazer.queries_path, params: {query: {name: "Test", statement: "SELECT 1", data_source: "main"}}
+    assert_response :redirect
+
+    query = Blazer::Query.last
+    get blazer.query_path(query)
+    assert_response :success
+
+    post blazer.run_queries_path, params: {statement: query.statement, data_source: query.data_source, query_id: query.id}, xhr: true
+    assert_response :success
+    audit = Blazer::Audit.last
+    assert_equal query.id, audit.query_id
+    assert_equal query.statement, audit.statement
+    assert_equal query.data_source, audit.data_source
+  end
+
+  # TODO switch to postgres and check table names
+  def test_tables
+    get blazer.tables_queries_path(data_source: "main")
     assert_response :success
   end
 end

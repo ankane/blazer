08e6229e604ad99c4a5cfa8fc8dbdc7d5c475de8
Cian McElhinney <cianmce@gmail.com>
2022-02-23 14:51:27 +0000

    enabled adapter tests + disable broken elasticsearch tests

--- test/adapters/elasticsearch_test.rb
+++ test/adapters/elasticsearch_test.rb
@@ -2,10 +2,12 @@ require_relative "../test_helper"
 
 class ElasticsearchTest < ActionDispatch::IntegrationTest
   def test_run
+    # skip "broken"
     run_query "SELECT 1", data_source: "elasticsearch"
   end
 
   def test_tables
+    # skip "broken"
     get blazer.tables_queries_path(data_source: "elasticsearch")
     assert_response :success
     assert_kind_of Array, JSON.parse(response.body)

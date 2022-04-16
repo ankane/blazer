d2202c755ccac656ea0d3f6d9f5ffda907c6a55a
Andrew Kane <andrew@ankane.org>
2022-04-15 21:35:22 -0700

    Improved Cassandra test [skip ci]

--- test/adapters/cassandra_test.rb
+++ test/adapters/cassandra_test.rb
@@ -1,14 +1,25 @@
 require_relative "../test_helper"
 
-# CREATE KEYSPACE blazer_test WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
-# USE blazer_test;
-# CREATE TABLE items (id int, name text, PRIMARY KEY (id));
-
 class CassandraTest < ActionDispatch::IntegrationTest
   include AdapterTest
 
   def data_source
     "cassandra"
+  end
+
+  def setup
+    @@once ||= begin
+      require "cassandra"
+      cluster = Cassandra.cluster(hosts: ["localhost"])
+
+      session = cluster.connect("system")
+      session.execute("CREATE KEYSPACE IF NOT EXISTS blazer_test WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }")
+
+      session = cluster.connect("blazer_test")
+      session.execute("DROP TABLE IF EXISTS items")
+      session.execute("CREATE TABLE items (id int, name text, PRIMARY KEY (id))")
+      true
+    end
   end
 
   def test_run

3fe1ab62d1a50f7166fae4e0d584cde8b85f3c3f
Andrew Kane <andrew@ankane.org>
2022-04-15 20:32:00 -0700

    Improved Cassandra test [skip ci]

--- test/adapters/cassandra_test.rb
+++ test/adapters/cassandra_test.rb
@@ -1,12 +1,17 @@
 require_relative "../test_helper"
 
-# CREATE KEYSPACE blazer_test
-# WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
+# CREATE KEYSPACE blazer_test WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
+# USE blazer_test;
+# CREATE TABLE items (id int, name text, PRIMARY KEY (id));
 
 class CassandraTest < ActionDispatch::IntegrationTest
   include AdapterTest
 
   def data_source
     "cassandra"
+  end
+
+  def test_run
+    assert_result [{"count" => "0"}], "SELECT COUNT(*) FROM items"
   end
 end

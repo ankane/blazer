d1d615f2cf7b2e762b354b68876e44096c47292d
Andrew Kane <andrew@ankane.org>
2022-04-15 18:03:07 -0700

    Improved Spark test [skip ci]

--- test/adapters/spark_test.rb
+++ test/adapters/spark_test.rb
@@ -1,9 +1,15 @@
 require_relative "../test_helper"
 
+# bin/beeline -u jdbc:hive2://localhost:10000 -e 'CREATE DATABASE blazer_test;'
+
 class SparkTest < ActionDispatch::IntegrationTest
   include AdapterTest
 
   def data_source
     "spark"
+  end
+
+  def test_run
+    assert_result [{"hello" => "world"}], "SELECT 'world' AS hello"
   end
 end

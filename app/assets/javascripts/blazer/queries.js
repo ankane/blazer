5222b68855f04e4227b0dc3231c2f48349364834
Andrew Kane <andrew@ankane.org>
2022-05-11 19:27:24 -0700

    Fixed query cancelation with Firefox and Safari [skip ci]

--- app/assets/javascripts/blazer/queries.js
+++ app/assets/javascripts/blazer/queries.js
@@ -53,7 +53,11 @@ function runQueryHelper(query) {
       queryComplete(query)
     }
   }).fail( function(jqXHR, textStatus, errorThrown) {
-    if (!query.canceled) {
+    // check jqXHR.status instead of query.canceled
+    // so it works for page navigation with Firefox and Safari
+    if (jqXHR.status === 0) {
+      cancelServerQuery(query)
+    } else {
       var message = (typeof errorThrown === "string") ? errorThrown : errorThrown.message
       if (!message) {
         message = "An error occurred"
@@ -86,6 +90,8 @@ function cancelAllQueries() {
   }
 }
 
+// needed for Chrome
+// queries are canceled before unload with Firefox and Safari
 $(window).on("unload", cancelAllQueries)
 
 function cancelQuery(query) {
@@ -93,7 +99,9 @@ function cancelQuery(query) {
   if (query.xhr) {
     query.xhr.abort()
   }
+}
 
+function cancelServerQuery(query) {
   // tell server
   var path = Routes.cancel_queries_path()
   var data = {run_id: query.run_id, data_source: query.data_source}

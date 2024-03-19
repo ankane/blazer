08795ba8f280f982683ea805f98ac1639a0e48c6
Nick Weiland <nickweiland@gmail.com>
2024-03-19 15:48:52 -0700

    Support for passing db config name to establish connection

--- lib/blazer/data_source.rb
+++ lib/blazer/data_source.rb
@@ -199,6 +199,10 @@ module Blazer
       end
     end
 
+    def sql_establish_connection_param
+      settings["sql_establish_connection_param"] || settings["url"]
+    end
+
     protected
 
     def adapter_instance

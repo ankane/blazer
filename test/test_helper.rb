16b1b6233af8546142d0799f32e3ce6361721d02
Andrew Kane <andrew@ankane.org>
2024-09-26 18:40:46 -0700

    Test with Rails 8.0.0.beta1 on CI [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ require "minitest/pride"
 logger = ActiveSupport::Logger.new(ENV["VERBOSE"] ? STDERR : nil)
 
 Combustion.path = "test/internal"
-Combustion.initialize! :active_record, :action_controller, :action_mailer, :active_job, :sprockets do
+Combustion.initialize! :active_record, :action_controller, :action_mailer, :active_job do
   config.load_defaults Rails::VERSION::STRING.to_f
   config.action_controller.logger = logger
   config.action_mailer.logger = logger

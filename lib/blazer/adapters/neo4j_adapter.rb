d0efc2b3f18173f417fe8a4f2db5c09043e521e6
Andrew Kane <andrew@ankane.org>
2025-02-22 19:23:56 -0800

    Added support for neo4j-ruby-driver gem [skip ci]

--- lib/blazer/adapters/neo4j_adapter.rb
+++ lib/blazer/adapters/neo4j_adapter.rb
@@ -7,14 +7,20 @@ module Blazer
         error = nil
 
         begin
-          result = session.query("#{statement} /*#{comment}*/", bind_params)
-          columns = result.columns.map(&:to_s)
-          rows = []
-          result.each do |row|
-            rows << columns.map do |c|
-              v = row.send(c)
-              v = v.properties if v.respond_to?(:properties)
-              v
+          if bolt?
+            result = session.run("#{statement} /*#{comment}*/", bind_params).to_a
+            columns = result.any? ? result.first.keys.map(&:to_s) : []
+            rows = result.map(&:values)
+          else
+            result = session.query("#{statement} /*#{comment}*/", bind_params)
+            columns = result.columns.map(&:to_s)
+            rows = []
+            result.each do |row|
+              rows << columns.map do |c|
+                v = row.send(c)
+                v = v.properties if v.respond_to?(:properties)
+                v
+              end
             end
           end
         rescue => e
@@ -26,8 +32,13 @@ module Blazer
       end
 
       def tables
-        result = session.query("CALL db.labels()")
-        result.rows.map(&:first)
+        if bolt?
+          result = session.run("CALL db.labels()").to_a
+          result.map { |r| r.values.first }
+        else
+          result = session.query("CALL db.labels()")
+          result.rows.map(&:first)
+        end
       end
 
       def preview_statement
@@ -52,10 +63,27 @@ module Blazer
 
       def session
         @session ||= begin
-          require "neo4j/core/cypher_session/adaptors/http"
-          http_adaptor = Neo4j::Core::CypherSession::Adaptors::HTTP.new(settings["url"])
-          Neo4j::Core::CypherSession.new(http_adaptor)
+          if bolt?
+            uri = URI.parse(settings["url"])
+            auth = Neo4j::Driver::AuthTokens.basic(uri.user, uri.password)
+            database = uri.path.delete_prefix("/")
+            uri.user = nil
+            uri.password = nil
+            uri.path = ""
+            Neo4j::Driver::GraphDatabase.driver(uri, auth).session(database: database)
+          else
+            require "neo4j/core/cypher_session/adaptors/http"
+            http_adaptor = Neo4j::Core::CypherSession::Adaptors::HTTP.new(settings["url"])
+            Neo4j::Core::CypherSession.new(http_adaptor)
+          end
         end
+      end
+
+      def bolt?
+        if !defined?(@bolt)
+          @bolt = settings["url"].start_with?("bolt")
+        end
+        @bolt
       end
     end
   end

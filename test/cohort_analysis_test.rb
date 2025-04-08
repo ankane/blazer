77d4c18e51ce21326e4ad4f68af9b9a1d9f75d8a
Andrew Kane <andrew@ankane.org>
2025-04-08 12:12:33 -0700

    Moved test [skip ci]

--- test/cohort_analysis_test.rb
+++ test/cohort_analysis_test.rb
@@ -11,15 +11,15 @@ class CohortAnalysisTest < ActionDispatch::IntegrationTest
     assert_match "1 cohort", response.body
   end
 
-  def test_missing_columns
-    run_query "SELECT 1 /* cohort analysis */", query_id: 1
-    assert_match "alert-danger", response.body
-  end
-
   def test_cohort_period_default
     query = create_query(statement: "SELECT 1 AS user_id, NOW() AS conversion_time /* cohort analysis */")
     get blazer.query_path(query)
     assert_response :success
     assert_match %{selected="selected" value="week"}, response.body
+  end
+
+  def test_missing_columns
+    run_query "SELECT 1 /* cohort analysis */", query_id: 1
+    assert_match "alert-danger", response.body
   end
 end

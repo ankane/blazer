d8cebe81b26c908381f70715efba9944863d38c4
Andrew Kane <andrew@ankane.org>
2026-05-20 14:14:19 -0700

    Improved tests for anomaly checks [skip ci]

--- test/anomaly_checks_test.rb
+++ test/anomaly_checks_test.rb
@@ -32,12 +32,20 @@ class AnomalyChecksTest < ActionDispatch::IntegrationTest
       Blazer.run_checks(schedule: "5 minutes")
       check.reload
       assert_equal "passing", check.state
+      assert_equal "No anomalies detected", check.message
 
-      query.update!(statement: "SELECT current_date + n AS day, 0.1 * random() FROM generate_series(1, 30) n UNION ALL SELECT current_date + 31, 2")
+      query.update!(statement: "SELECT current_date + n AS day, 0.1 * random() AS v FROM generate_series(1, 30) n UNION ALL SELECT current_date + 31, 2 AS v")
 
       Blazer.run_checks(schedule: "5 minutes")
       check.reload
       assert_equal "failing", check.state
+      assert_equal "Anomaly detected in v", check.message
+
+      query.update!(statement: "SELECT 1")
+      Blazer.run_checks(schedule: "5 minutes")
+      check.reload
+      assert_equal "error", check.state
+      assert_equal "Bad format", check.message
     end
   end
 end

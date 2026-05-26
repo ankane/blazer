6e0108f25993eff3419d502fe75f7bfc91450cea
Andrew Kane <andrew@ankane.org>
2026-05-26 08:02:27 -0700

    Added query cancellation for ClickHouse [skip ci]

--- lib/blazer/adapters/clickhouse_adapter.rb
+++ lib/blazer/adapters/clickhouse_adapter.rb
@@ -2,49 +2,12 @@ module Blazer
   module Adapters
     class ClickhouseAdapter < BaseAdapter
       def run_statement(statement, comment, bind_params)
-        require "json"
-        require "net/http"
-
         columns = []
         rows = []
         error = nil
 
-        query_params = {
-          "database" => database,
-          "default_format" => "JSONCompact",
-          "output_format_json_quote_64bit_integers" => 0, # for ClickHouse < 25.8
-          "readonly" => 1
-        }
-        query_params["max_execution_time"] = data_source.timeout if data_source.timeout
-
-        post_data = {
-          "query" => "#{statement} /*#{comment}*/"
-        }
-        bind_params.each do |k, v|
-          v = v.utc.strftime("%Y-%m-%d %H:%M:%S.%N") if v.is_a?(ActiveSupport::TimeWithZone)
-          # https://github.com/ClickHouse/ClickHouse/issues/69656
-          v = v.gsub("\\") { "\\\\" } if v.is_a?(String)
-          post_data["param_#{k}"] = v.to_s unless v.nil?
-        end
-
-        uri = URI(settings["url"])
-        raise "Not supported" if uri.query
-        uri.query = URI.encode_www_form(query_params)
-
-        req = Net::HTTP::Post.new(uri)
-        req.basic_auth(uri.user, uri.password) if uri.user || uri.password
-        req.set_form(post_data, "multipart/form-data")
-
-        options = {
-          use_ssl: uri.scheme == "https",
-          open_timeout: 3,
-          read_timeout: data_source.timeout ? data_source.timeout + 3 : 30
-        }
-
         begin
-          res = Net::HTTP.start(uri.hostname, uri.port, options) do |http|
-            http.request(req)
-          end
+          res = execute("#{statement} /*#{comment}*/", bind_params)
 
           if res.is_a?(Net::HTTPSuccess)
             data = JSON.parse(res.body)
@@ -119,10 +82,55 @@ module Blazer
         end
       end
 
+      def cancel(run_id)
+        execute("KILL QUERY WHERE query LIKE {query: String} AND query NOT LIKE '%system.processes%' SYNC", {"query" => "%,run_id:#{run_id}%"})
+      end
+
       private
 
       def database
         @database ||= settings["database"] || "default"
+      end
+
+      def execute(statement, bind_params)
+        require "json"
+        require "net/http"
+
+        query_params = {
+          "database" => database,
+          "default_format" => "JSONCompact",
+          "output_format_json_quote_64bit_integers" => 0, # for ClickHouse < 25.8
+          "readonly" => 1
+        }
+        query_params["max_execution_time"] = data_source.timeout if data_source.timeout
+
+        post_data = {
+          "query" => statement
+        }
+        bind_params.each do |k, v|
+          v = v.utc.strftime("%Y-%m-%d %H:%M:%S.%N") if v.is_a?(ActiveSupport::TimeWithZone)
+          # https://github.com/ClickHouse/ClickHouse/issues/69656
+          v = v.gsub("\\") { "\\\\" } if v.is_a?(String)
+          post_data["param_#{k}"] = v.to_s unless v.nil?
+        end
+
+        uri = URI(settings["url"])
+        raise "Not supported" if uri.query
+        uri.query = URI.encode_www_form(query_params)
+
+        req = Net::HTTP::Post.new(uri)
+        req.basic_auth(uri.user, uri.password) if uri.user || uri.password
+        req.set_form(post_data, "multipart/form-data")
+
+        options = {
+          use_ssl: uri.scheme == "https",
+          open_timeout: 3,
+          read_timeout: data_source.timeout ? data_source.timeout + 3 : 30
+        }
+
+        Net::HTTP.start(uri.hostname, uri.port, options) do |http|
+          http.request(req)
+        end
       end
     end
   end

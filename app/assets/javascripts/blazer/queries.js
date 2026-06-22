61652754c690a3df11ae9d12334e868dea8e3676
Andrew Kane <andrew@ankane.org>
2026-06-22 14:45:44 -0700

    Updated style [skip ci]

--- app/assets/javascripts/blazer/queries.js
+++ app/assets/javascripts/blazer/queries.js
@@ -79,7 +79,8 @@ function uuid() {
   }
 
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    const r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8)
+    const r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8
     return v.toString(16)
   })
 }

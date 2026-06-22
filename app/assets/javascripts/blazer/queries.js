1555b1d072971a5328fccd92f8d09445fdbac104
Andrew Kane <andrew@ankane.org>
2026-06-22 14:43:00 -0700

    Improved UUID generation [skip ci]

--- app/assets/javascripts/blazer/queries.js
+++ app/assets/javascripts/blazer/queries.js
@@ -74,6 +74,10 @@ function queryComplete(query) {
 }
 
 function uuid() {
+  if (window.crypto && window.crypto.randomUUID) {
+    return window.crypto.randomUUID()
+  }
+
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     const r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8)
     return v.toString(16)

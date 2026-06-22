c062ae4d6edfeb5d243f4d844b0cdb6a1c4569f1
Andrew Kane <andrew@ankane.org>
2026-06-21 19:20:05 -0700

    Updated style [skip ci]

--- app/assets/javascripts/blazer/routes.js
+++ app/assets/javascripts/blazer/routes.js
@@ -1,26 +1,26 @@
 var Routes = {
-  run_queries_path: function() {
+  run_queries_path: function () {
     return rootPath + "queries/run"
   },
-  cancel_queries_path: function() {
+  cancel_queries_path: function () {
     return rootPath + "queries/cancel"
   },
-  schema_queries_path: function(params) {
+  schema_queries_path: function (params) {
     return rootPath + "queries/schema?" + pathParams(params)
   },
-  docs_queries_path: function(params) {
+  docs_queries_path: function (params) {
     return rootPath + "queries/docs?" + pathParams(params)
   },
-  tables_queries_path: function(params) {
+  tables_queries_path: function (params) {
     return rootPath + "queries/tables?" + pathParams(params)
   },
-  queries_path: function() {
+  queries_path: function () {
     return rootPath + "queries"
   },
-  query_path: function(id) {
+  query_path: function (id) {
     return rootPath + "queries/" + id
   },
-  dashboard_path: function(id) {
+  dashboard_path: function (id) {
     return rootPath + "dashboards/" + id
   }
 }
